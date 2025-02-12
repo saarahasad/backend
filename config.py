d1f755146b496366fab387822d74a31e15c19494
Saarah Asad <saarahasad26@gmail.com>
2025-02-12 17:07:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-DATABASE_URL = "postgresql://scraberdb_user:EV2D5VDBzTsG79iG8FPdoUQtfDo32yVe@dpg-cukas2d6l47c73c9m420-a/scraberdb"
+DATABASE_URL = "psql postgresql://scraberdb_user:EV2D5VDBzTsG79iG8FPdoUQtfDo32yVe@dpg-cukas2d6l47c73c9m420-a.oregon-postgres.render.com/scraberdb"
